11a4191517a1dcb12b4aa2dde4d2218004d3e0a2
Jimmy Wu <jimmywu0605@gmail.com>
2022-11-08 16:02:06 -0500

    add .zsh_profile

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ln -sfv ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sfv ~/.dotfiles/zsh/.zsh_profile ~/.zsh_profile
 ln -sfv ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sfv ~/.dotfiles/vim/.vimrc ~/.vimrc
